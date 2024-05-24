fee7fd785e75f73261d6bbb5595b34d770c207c2
G0LDF0X <astnv31@gmail.com>
2024-05-24 15:45:25 +0900

    modify

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# lionAir_project
+# Like Lion 프론트-백엔드 연결 미니 프로젝트
+
+해당 코드는 Serializer가 아닌 FastAPI를 이용해 만들어졌습니다.
+
+시간 부족으로 코드 정리 및 최적화가 덜 되어 있습니다...
+
+사이트 링크 : https://lion-air-project.vercel.app/
--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://100.27.31.137:8080',
 });
 
 axiosInstance.interceptors.request.use((config) => {
